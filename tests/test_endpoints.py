5e0d127d24a6df22758f6cbaa316dc5d1a3502a6
Henry Iwanejko <henry.iwanejko1@nhs.net>
2021-03-17 09:45:38 +0000

    APM-1910 add print statement

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -75,5 +75,6 @@ class TestEndpoints:
     def test_ping_endpoint(self):
         """Test that the ping endpoint returns 200"""
         response = requests.get(f"{config.BASE_URL}/{config.PROXY}/_ping")
+        print(f"{config.BASE_URL}/{config.PROXY}/_ping")
         assert response.status_code == 200
         assert list(response.json().keys()) == ['version', 'revision', 'releaseId', 'commitId']
